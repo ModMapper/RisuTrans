4f3c4f4398a2fed9c83f47b9c8876ede68be8be5
ModMapper <modmapper@tkyuki.kr>
2023-11-20 02:16:50 +0900

    엔터키 처리 추가

--- firefox/scripts/risu.js
+++ firefox/scripts/risu.js
@@ -1,5 +1,5 @@
-(async function() {
-    if(window._RISU_TRANS) return;
+(async function () {
+    if (window._RISU_TRANS) return;
     window._RISU_TRANS = true;
 
     function CreateTranslatorAsync() {
@@ -16,7 +16,7 @@
         function CreateFrameAsync() {
             return new Promise((resolve) => {
                 const frame = document.createElement("iframe");
-                frame.src = "https://www.deepl.com/translator";
+                frame.src = MobileCheck() ? "https://www.deepl.com/translator-mobile" : "https://www.deepl.com/translator";
                 frame.tabIndex = -1;
                 frame.style = "width: 500px; height: 500px; border: none;";
                 frame.onload = () => resolve(frame);
@@ -28,13 +28,13 @@
         }
         function GetTranslator(port) {
             let lastTask = Promise.resolve();
-            return function(text, source, target) {
+            return function (text, source, target) {
                 const task = lastTask;
                 return lastTask = new Promise(async (resolve) => {
                     await task;
-                    console.log({text: text, source: source, target: target});
+                    console.log({ text: text, source: source, target: target });
                     port.addEventListener("message", (evt) => resolve(evt.data), { once: true });
-                    port.postMessage({text: text, source: source, target: target});
+                    port.postMessage({ text: text, source: source, target: target });
                 });
             }
         }
@@ -46,39 +46,68 @@
 
     document.addEventListener("click", () => {
         const CheckCode = "CHAT_TRANS";
-        const txtRisuTrans = document.getElementById("messageInputTranslate");
-        if(txtRisuTrans == null || txtRisuTrans.hasAttribute(CheckCode)) return;
+        const txtTransOld = document.getElementById("messageInputTranslate");
+        if (txtTransOld == null || txtTransOld.hasAttribute(CheckCode)) return;
 
-        const txtTrans = txtRisuTrans.cloneNode(true);
+        const txtTrans = txtTransOld.cloneNode(true);
         txtTrans.setAttribute(CheckCode, "");
-        txtRisuTrans.after(txtTrans);
-        txtRisuTrans.remove();
+        txtTransOld.after(txtTrans);
+        txtTransOld.remove();
 
-        const txtChat = document.querySelector(".default-chat-screen > div:first-child textarea");
+        const txtChatOld = document.querySelector(".default-chat-screen > div:first-child textarea");
+        const buttonArea = txtChatOld.parentElement;
+
+        const txtChat = txtChatOld.cloneNode(true);
+        txtChatOld.after(txtChat);
+        txtChatOld.remove();
 
         DelayValueCheck(txtTrans, async (value) => {
             txtChat.value = await translate(value, null, "EN");
-            const evt = new Event('input', { bubbles: true });
-            evt.break = true;
-            txtChat.dispatchEvent(evt);
+            txtChatOld.value = txtChat.value;
+            txtChatOld.dispatchEvent(new Event('input', { bubbles: true }));
         });
         DelayValueCheck(txtChat, async (value) => {
             txtTrans.value = await translate(value, null, "KO");
         });
+        txtChat.addEventListener("input", () => {
+            txtChatOld.value = txtChat.value;
+            txtChatOld.dispatchEvent(new Event('input', { bubbles: true }));
+        });
+
+        txtTrans.addEventListener("keydown", EnterEvent);
+        txtChat.addEventListener("keydown", EnterEvent);
+        buttonArea.addEventListener("click", (evt) => {
+            const buttonIcon = buttonArea.querySelector(".lucide-send");
+            if (buttonIcon != null && buttonIcon.parentElement.contains(evt.target)) {
+                txtTrans.value = txtChat.value = "";
+            }
+        })
+
+        function EnterEvent(evt) {
+            if (evt.keyCode == 13) {
+                buttonArea.querySelector(".lucide-send").parentElement.click();
+                evt.preventDefault();
+            }
+        }
     });
 
     function DelayValueCheck(input, callback) {
         let value = input.value;
-        let changed = false;
-        setInterval(() => {
-            const current = input.value
-            if(current != value && changed) {
-                value = current;
-                callback(current);
+        input.addEventListener("input", Check, { once: true });
+
+        function Check() {
+            const current = input.value;
+            if (value != current) {
+                callback(value = current);
+                setTimeout(Check, 1000);
+            } else {
+                input.addEventListener("input", Check, { once: true });
             }
-            changed = false;
-        }, 1000);
-        input.addEventListener("input", (evt) => changed = changed || !evt.break);
+        }
+    }
+
+    function MobileCheck() {
+        return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
     }
 })();
 
